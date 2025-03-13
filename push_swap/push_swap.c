03f05d29cb3a4d5667318eb033b5e27c5f8e1300
Edgar Arutiunian <earutiun@student.42berlin.de>
2025-03-13 14:20:15 +0100

    this is just the beggining...

---
+++ push_swap/push_swap.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: earutiun <earutiun@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/13 13:22:35 by earutiun          #+#    #+#             */
+/*   Updated: 2025/03/13 13:22:41 by earutiun         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int main(int argc, char *argv[])
+{
+    t_stack *first;
+
+    first = create_list(argc, argv);
+    return (0);
+}
+
+t_stack *create_list(argc, argv)
+{
+    int i;
+    t_stack first;
+
+    i = 0;
+    while (i < argc)
+    {
+        
+        i++;
+    }
+}
---
+++ push_swap/push_swap.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: earutiun <earutiun@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/13 14:01:52 by earutiun          #+#    #+#             */
+/*   Updated: 2025/03/13 14:02:02 by earutiun         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include "./libft/libft.h"
+
+typedef struct s_stack
+{
+    int x;
+    struct s_stack *next;
+    struct s_stack *prev;
+} t_stack;
+
+#endif
