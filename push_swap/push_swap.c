0a01b4968c26b5c312c2731f63406d872ca37fbf
Edgar Arutiunian <earutiun@student.42berlin.de>
2025-03-14 13:07:21 +0100

    create_list, init_struct

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -23,12 +23,37 @@ int main(int argc, char *argv[])
 t_stack *create_list(argc, argv)
 {
     int i;
-    t_stack first;
+    t_stack *first;
+    t_stack *temp;
 
+    first = NULL;
     i = 0;
     while (i < argc)
     {
-        
+        temp = (t_stack *)ft_calloc(1, sizeof(t_stack));
+        if (!temp)
+            exit (-1);
+        init_struct(first, temp);
+        if (!first)
+            first = temp;
         i++;
     }
+}
+
+void    init_struct(t_stack *first, t_stack *new)
+{
+    t_stack *curr;
+
+    if (!first)
+    {
+        new->prev = NULL;
+        new->next = NULL;
+        return ;
+    }
+    curr = first;
+    while (curr->next)
+        curr = curr->next;
+    curr->next = new;
+    new->prev = curr;
+    new->next = NULL;
 }
